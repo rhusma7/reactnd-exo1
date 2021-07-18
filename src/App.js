1d762b43f47137adc378a1d0ddcf2e08b65d0c8b
root <mariusnguessan5@gmail.com>
2021-07-18 22:54:37 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,101 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
+import FavoriteMovies from './FavoriteMovies.js';
+import { profiles, users, movies } from './Data.js';
 
 /*
 Use React and the data below to display a list of users alongside their favorite movies.
 
 For detailed instructions, refer to instructions.md.
 */
-
-const profiles = [
-  {
-    id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
-  },
-  {
-    id: 2,
-    userID: '2',
-    favoriteMovieID: '1',
-  },
-  {
-    id: 3,
-    userID: '4',
-    favoriteMovieID: '5',
-  },
-  {
-    id: 4,
-    userID: '5',
-    favoriteMovieID: '2',
-  },
-  {
-    id: 5,
-    userID: '3',
-    favoriteMovieID: '5',
-  },
-  {
-    id: 6,
-    userID: '6',
-    favoriteMovieID: '4',
-  },
-];
-
-const users = {
-  1: {
-    id: 1,
-    name: 'Jane Cruz',
-    userName: 'coder',
-  },
-  2: {
-    id: 2,
-    name: 'Matthew Johnson',
-    userName: 'mpage',
-  },
-  3: {
-    id: 3,
-    name: 'Autumn Green',
-    userName: 'user123',
-  },
-  4: {
-    id: 4,
-    name: 'John Doe',
-    userName: 'user123',
-  },
-  5: {
-    id: 5,
-    name: 'Lauren Carlson',
-    userName: 'user123',
-  },
-  6: {
-    id: 6,
-    name: 'Nicholas Lain',
-    userName: 'user123',
-  },
-};
-
-const movies = {
-  1: {
-    id: 1,
-    name: 'Planet Earth 1',
-  },
-  2: {
-    id: 2,
-    name: 'Selma',
-  },
-  3: {
-    id: 3,
-    name: 'Million Dollar Baby',
-  },
-  4: {
-    id: 4,
-    name: 'Forrest Gump',
-  },
-  5: {
-    id: 5,
-    name: 'Get Out',
-  },
-};
+// eslint-disable-next-line
 
 class App extends Component {
   render() {
@@ -106,7 +20,11 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<FavoriteMovies profiles={profiles} users={users} movies={movies} />
       </div>
+		
+	
+	
     );
   }
 }
